c44f99063cb60aaed643f6d0928153bd20e525d2
shraazp <shravyap99@gmail.com>
2022-02-11 12:21:58 +0530

    refactored:unnecessary code

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ Redirects to different pages
 @returns home page
 @author:Shravya P
  */
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
-=======
-import React from 'react';
->>>>>>> 7d2d08d96a24b54cba1e253485d6cc6ccbda7aa4
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
-<<<<<<< HEAD
 import { useDispatch } from 'react-redux';
 import Home from './pages/home/Home';
 import Products from './pages/products/Products';
 import SingleProduct from './pages/singleProduct/SingleProduct';
 import { getToken } from './utils/Common';
 import { loginStatus } from './actions/categoryActions';
-=======
-import Home from './pages/home/Home';
-import Products from './pages/products/Products';
-import SingleProduct from './pages/singleProduct/SingleProduct';
->>>>>>> 7d2d08d96a24b54cba1e253485d6cc6ccbda7aa4
 
 function App() {
   const dispatch = useDispatch();
